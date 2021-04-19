216dd3590548b5893c800837271f4b26c1f43a99
e71828 <740943170@qq.com>
2021-04-19 22:03:59 +0800

    Create entrypoint.sh

---
+++ rstp-server/entrypoint.sh
@@ -0,0 +1,8 @@
+# install rtsp
+sudo apt-get install libgstrtspserver-1.0-dev gstreamer1.0-rtsp
+
+# compile code
+gcc -o stream  stream.c  `pkg-config --cflags --libs gstreamer-rtsp-server-1.0`
+
+# execute code
+./stream "( filesrc location=/src/sample-mp4-file.mp4 ! qtdemux ! h264parse ! rtph264pay name=pay0 pt=96 )"
