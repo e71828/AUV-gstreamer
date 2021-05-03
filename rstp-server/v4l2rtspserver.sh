290b8a2419fcabaef850162551240b030fada23c
e71828 <740943170@qq.com>
2021-05-03 18:14:42 +0800

    Update v4l2rtspserver.sh

--- rstp-server/v4l2rtspserver.sh
+++ rstp-server/v4l2rtspserver.sh
@@ -1,7 +1,7 @@
 sudo apt-get install cmake liblog4cpp5-dev libv4l-dev libssl-dev liblivemedia-dev openssl
 git clone https://github.com/mpromonet/v4l2rtspserver.git
 cd v4l2rtspserver
-git config --global https_proxy https://192.168.31.163:7890
+git config --global https.proxy https://192.168.31.163:7890
 # modify the CMakeLists.txt
 sed -i /^\s*EXEC_PROGRAM("git/ c\EXEC_PROGRAM("git submodule update --init --depth=1") CMakeLists.txt
 cmake -H. -Bbuild
